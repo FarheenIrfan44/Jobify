0c860f2b5e8a4f8966babaf3b3ca59c3403aed99
FarheenIrfan44 <farheenirfan4040@gmail.com>
2024-10-05 18:26:06 +0500

    updates

--- app/index.js
+++ app/index.js
@@ -1,10 +1,34 @@
-import { Text, View } from "react-native";
+import { Text, View, ScrollView, SafeAreaView } from "react-native";
+import { useState } from "react";
+import { Stack, useRouter } from "expo-router";
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 const Home = () => {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Hello World !</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () =>(
+            <ScreenHeaderBtn iconUrl = {icons.menu} dimention = "60%" />
+          ),
+          headerRight: () =>(
+            <ScreenHeaderBtn iconUrl = {icons.menu} dimention = "60%" />
+          ),
+          headerTitle: ""
+
+        }}
+      />
+    </SafeAreaView>
   );
 };
 
