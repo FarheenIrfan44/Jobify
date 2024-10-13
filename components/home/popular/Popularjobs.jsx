e079496ccb7655f1e47977e8ec420788aabcab75
FarheenIrfan44 <farheenirfan4040@gmail.com>
2024-10-13 13:04:38 +0500

    commit

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { useState } from "react";
+import { useRouter } from "expo-router";
 import {
   View,
   Text,
@@ -6,51 +7,53 @@ import {
   FlatList,
   ActivityIndicator,
 } from "react-native";
-import { useState } from "react";
-import { useRouter } from "expo-router";
 
 import styles from "./popularjobs.style";
 import { COLORS, SIZES } from "../../../constants";
 import PopularJobCard from "../../common/cards/popular/PopularJobCard";
-import useFetch from '../../../hook/useFetch';
+import useFetch from "../../../hook/useFetch";
 
 const Popularjobs = () => {
   const router = useRouter();
+  const { data, isLoading, error } = useFetch("search", {
+    query: "React developer",
+    num_pages: "1",
+  });
 
-  const {data, isLoading,error} = useFetch('search',{
-    query: 'React developer',
-    num_page: 1
-    })
+  const [selectedJob, setSelectedJob] = useState();
 
-    console.log(data);
- 
-
-  const jobs = [1,2,3,4]; // Dummy job data (replace with your data)
+  const handleCardPress = (item) => {
+    router.push(`/job-details/${item.job_id}`);
+    setSelectedJob(item.job_id);
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={styles.headerTitle}>Popular Jobs</Text>
+        <Text style={styles.headerTitle}>Popular jobs</Text>
         <TouchableOpacity>
-          <Text style={styles.headerBtn}>Show All</Text>
+          <Text style={styles.headerBtn}>Show all</Text>
         </TouchableOpacity>
       </View>
+
       <View style={styles.cardsContainer}>
         {isLoading ? (
-          <ActivityIndicator size="large" color={COLORS.primary} />
+          <ActivityIndicator size='large' color={COLORS.primary} />
         ) : error ? (
           <Text>Something went wrong</Text>
         ) : (
           <FlatList
-            data={jobs} // replace with your job data
+            data={data}
             renderItem={({ item }) => (
-              <PopularJobCard item={item} /> // Your custom job card component
+              <PopularJobCard
+                item={item}
+                selectedJob={selectedJob}
+                handleCardPress={handleCardPress}
+              />
             )}
-            keyExtractor={item => item?.job_id} 
+            keyExtractor={(item) => item.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
             horizontal
-            contentContainerStyle={{columnGap: SIZES.medium}}
-            showsHorizontalScrollIndicator={false}
-            scrollEnabled={true} 
           />
         )}
       </View>
