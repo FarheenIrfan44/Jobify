0bf5e701075a8f0922246f6c224d62a7a5ddb89f
FarheenIrfan44 <farheenirfan4040@gmail.com>
2024-10-13 11:33:59 +0500

    commit

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,8 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { RPID_API_KEY } from "@env";
 
-const rapidApiKey = RPID_API_KEY;
 
 const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
@@ -13,22 +11,12 @@ const useFetch = (endpoint, query) => {
     method: "GET",
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "X-RapidAPI-Key": rapidApiKey,
+      "X-RapidAPI-Key": "f11e434301mshed1c8c8c2912cc7p1bc3bdjsn2c9f6f489ae7",
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
     params: { ...query },
   };
 
-  const options1 = {
-    method: "GET",
-    hostname: "jsearch.p.rapidapi.com",
-    port: null,
-    path: "/estimated-salary?job_title=NodeJS%20Developer&location=New-York%2C%20NY%2C%20USA&radius=100",
-    headers: {
-      "x-rapidapi-key": "f11e434301mshed1c8c8c2912cc7p1bc3bdjsn2c9f6f489ae7",
-      "x-rapidapi-host": "jsearch.p.rapidapi.com",
-    },
-  };
 
   const fetchData = async () => {
     setIsLoading(true);
